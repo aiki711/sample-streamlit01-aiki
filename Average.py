ea81c96c62e7a83e29d223243daa3f35c3989ed7
aiki <aiking.yamagami@gmail.com>
2021-05-07 20:10:19 +0900

    1st commit

--- Average.py
+++
@@ -1,4 +0,0 @@
-def average(num01, num02, num03):
-  x = num01 + num02 + num03
-  return(x/3)
-print(average(2, 4, 9))
--- main.py
+++ main.py
@@ -20,22 +20,23 @@ if uploaded_file is not None:
         img.save(output, format='JPEG')
         binary_img = output.getvalue()
     
-    headers = {
+        headers = {
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': subscription_key
-      }
+    }
 
-    params = {
+        params = {
         'returnFaceId': 'true', 'returnFaceAttributes':'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
     
-      }
+    }
 
-    res = requests.post(face_api_url, params=params, headers=headers,data=binary_img)
+        res = requests.post(face_api_url, params=params, headers=headers,data=binary_img)
 
-    results = res.json()
-    for result in results:
-        rect = result['faceRectangle']
-        draw = ImageDraw.Draw(img)
-        draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='blue', width=5)
+        results = res.json()
+        for result in results:
+            rect = result['faceRectangle']
+    
+            draw = ImageDraw.Draw(img)
+            draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='blue', width=5)
 
     st.image(img, caption='Uploaded Image.', use_column_width=True)
